4c1e64fa23c1e447dc30234ae9ad39490de8bfec
christi4n <christian@mydevfactory.net>
2022-04-12 09:52:23 +0200

    [FIX] Correct init problem

--- src/main/java/net/mydevfactory/payroll/PayrollApplication.java
+++ src/main/java/net/mydevfactory/payroll/PayrollApplication.java
@@ -2,14 +2,21 @@ package net.mydevfactory.payroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 // fire up a servlet container 
 // and serve up our service
 @SpringBootApplication
-public class PayrollApplication {
+public class PayrollApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PayrollApplication.class, args);
 	}
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(PayrollApplication.class);
+    }
 
 }
